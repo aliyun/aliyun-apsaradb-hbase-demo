3b88ff04998aebd47c4aa63386d1c4a9b7437364
cm <cm@mingshuodeMacBook-Pro.local>
2018-12-11 17:39:52 +0800

    refine TestPhoenixJDBC, add Timestamp type

--- phoenix/src/main/java/com/aliyun/phoenix/TestPhoenixJDBC.java
+++ phoenix/src/main/java/com/aliyun/phoenix/TestPhoenixJDBC.java
@@ -1,11 +1,6 @@
 package com.aliyun.phoenix;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Properties;
 
 /**
@@ -21,19 +16,36 @@ public class TestPhoenixJDBC {
             Connection con =
                     DriverManager.getConnection("jdbc:phoenix:" + zkAddress, p);
             Statement stmt = con.createStatement();
-            stmt.execute("create table if not exists test (mykey integer not null primary key, mycolumn varchar) salt_buckets = 4");
-            stmt.execute("create index if not exists test_idx on test(mycolumn)");
-            stmt.executeUpdate("upsert into test values (1,'World!')");
-            stmt.executeUpdate("upsert into test values (2,'Hello')");
-            stmt.executeUpdate("upsert into test values (3,'World!')");
-            con.commit();
-            PreparedStatement statement = con.prepareStatement("select mykey from test where mycolumn='Hello'");
-            ResultSet rset = statement.executeQuery();
-            while (rset.next()) {
-                System.out.println(rset.getInt(1));
-            }
+            stmt.execute("create table if not exists test (mykey integer not null primary key, mycolumn varchar, mytime timestamp) salt_buckets = 4");
+            stmt.execute("create index if not exists test_idx on test(mycolumn) include (mytime)");
             stmt.close();
+            con.commit();
+
+            PreparedStatement pstmt = con.prepareStatement("upsert into test(mykey, mycolumn, mytime) values(?, ?, ?)");
+
+            pstmt.setInt(1, 1);
+            pstmt.setString(2, "World!");
+            pstmt.setTimestamp(3, Timestamp.valueOf("2018-12-11 10:10:10"));
+            pstmt.executeUpdate();
+
+            pstmt.setInt(1, 2);
+            pstmt.setString(2, "Hello");
+            pstmt.setTimestamp(3, Timestamp.valueOf("2018-12-11 10:10:20"));
+            pstmt.executeUpdate();
+
+            pstmt.setInt(1, 3);
+            pstmt.setString(2, "World!");
+            pstmt.setTimestamp(3, Timestamp.valueOf("2018-12-11 10:10:30"));
+            pstmt.executeUpdate();
+
+            con.commit();
+
+            ResultSet rset = pstmt.executeQuery("select mykey, mytime from test where mycolumn='Hello'");
+            while (rset.next()) {
+                System.out.println(rset.getInt(1) + " : " + rset.getTimestamp(2));
+            }
             rset.close();
+            pstmt.close();
             con.close();
         } catch (SQLException e) {
             e.printStackTrace();
