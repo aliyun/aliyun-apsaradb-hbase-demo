b237e47ef032000b901788074f069bd68f1eaf22
bill1208 <zgcsky08@163.com>
2019-04-16 22:27:57 +0800

    modify the file name of spark on phoenix

--- spark/spark-examples/src/main/java/com/aliyun/spark/JavaSparkOnPhoenix5xSparkSession.java
+++ spark/spark-examples/src/main/java/com/aliyun/spark/JavaSparkOnPhoenix5xSparkSession.java
@@ -3,24 +3,23 @@ package com.aliyun.spark;
 import org.apache.spark.sql.SparkSession;
 
 /**
- * 运行命令
- * spark-submit --master yarn --class com.aliyun.spark.JavaSparkOnPhoenix5xSparkSession /opt/spark-examples-0.0.1-SNAPSHOT.jar zkAddress
+ * 本实例适用于Phoenix 5.x版本
  */
 public class JavaSparkOnPhoenix5xSparkSession {
   public static void main(String[] args) {
+    //queryServerAddress为HBase集群SQL服务访问地址，格式：http://xxx：8765
+    String queryServerAddress = args[0];
+    //Phoenix侧的表名，需要在Phoenix侧提前创建。Phoenix表创建可以参考：https://help.aliyun.com/document_detail/53716.html?spm=a2c4g.11186623.4.2.4e961ff0lRqHUW
+    String phoenixTableName = args[1];
+    //Spark侧的表名。
+    String sparkTableName = args[2];
+
     SparkSession sparkSession = SparkSession
             .builder()
             .enableHiveSupport() //使用hive-metastore后通过beenline可以查看到代码中创建的表
             .appName("java spark on Phoenix5.x test")
             .getOrCreate();
 
-    //Spark侧的表名。
-    String sparkTableName = "spark_phoenix";
-    //Phoenix侧的表名，需要在Phoenix侧提前创建。Phoenix表创建可以参考：https://help.aliyun.com/document_detail/53716.html?spm=a2c4g.11186623.4.2.4e961ff0lRqHUW
-    String phoenixTableName = "us_population";
-
-    //queryServerAddress为HBase集群SQL服务访问地址，格式：http://xxx：8765
-    String queryServerAddress = args[0];
     String url = "jdbc:phoenix:thin:url=" + queryServerAddress + ";serialization=PROTOBUF";
     String driver = "org.apache.phoenix.queryserver.client.Driver";
     String createCmd = "CREATE TABLE " +
