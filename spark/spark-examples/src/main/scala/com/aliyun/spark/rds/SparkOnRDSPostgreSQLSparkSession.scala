6d4dde1ec9bdb32ce52dbd55da4cfdbfedb92c6d
bill1208 <zgcsky08@163.com>
2019-04-19 10:55:31 +0800

    add the testcase for spark on rds

---
+++ spark/spark-examples/src/main/scala/com/aliyun/spark/rds/SparkOnRDSPostgreSQLSparkSession.scala
@@ -0,0 +1,74 @@
+package com.aliyun.spark.rds
+
+import java.util.Properties
+
+import com.aliyun.spark.polardb.Person
+import org.apache.spark.sql.SparkSession
+
+object SparkOnRDSPostgreSQLSparkSession {
+
+  def main(args: Array[String]): Unit = {
+    //获取POLARDB的 url、database、tableName、登录POLARDB数据库的user和password
+    val url = args(0)
+    val database = args(1)
+    val jdbcConnURL = s"jdbc:postgresql://$url/$database"
+    var schemaName = args(2)
+    val tableName = args(3)
+    val user = args(4)
+    val password = args(5)
+
+    //Spark侧的表名。
+    var sparkTableName = args(6)
+
+    val sparkSession = SparkSession
+      .builder()
+      .enableHiveSupport() //可选，使用hive-metastore后通过thriftServer可以查看到代码中创建的表
+      .appName("scala spark on RDS test")
+      .getOrCreate()
+
+    val driver = "org.postgresql.Driver"
+
+    //Sql方式，Spark会映射POLARDB中表的Schema。
+    val createCmd =
+      s"""CREATE TABLE ${sparkTableName} USING org.apache.spark.sql.jdbc
+         |    options (
+         |    driver '$driver',
+         |    url '$jdbcConnURL',
+         |    dbtable '$schemaName.$tableName',
+         |    user '$user',
+         |    password '$password'
+         |    )""".stripMargin
+    println(s"createCmd: \n $createCmd")
+    sparkSession.sql(createCmd)
+    val querySql = "select * from " + sparkTableName + " limit 1"
+    sparkSession.sql(querySql).show
+
+
+    //使用dataset API接口
+    val connectionProperties = new Properties()
+    connectionProperties.put("driver", driver)
+    connectionProperties.put("user", user)
+    connectionProperties.put("password", password)
+    //读取数据
+    var jdbcDf = sparkSession.read.jdbc(jdbcConnURL,
+      s"$database.$schemaName.$tableName",
+      connectionProperties)
+    jdbcDf.select("name", "age", "score").show()
+
+    val data =
+      Seq(
+        Person("bill", 30, 170.5D),
+        Person("gate", 29, 200.3D)
+      )
+    val dfWrite = sparkSession.createDataFrame(data)
+
+    //写入数据
+    dfWrite
+      .write
+      .mode("append")
+      .jdbc(jdbcConnURL, s"$database.$schemaName.$tableName", connectionProperties)
+    jdbcDf.select("name", "age").show()
+    sparkSession.stop()
+  }
+
+}
